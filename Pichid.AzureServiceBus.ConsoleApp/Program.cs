69c06e6b531cdc1b40499a65512745016f65ad76
pichid.de <pichid.de@hengleasing.co.th>
2023-05-03 22:43:01 +0700

    first commit

---
+++ Pichid.AzureServiceBus.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+var credentialOptions = new VisualStudioCredentialOptions
+{
+    TenantId = "3b4746c8-cc8f-4413-8e2c-5dad5c063418"
+};
+TokenCredential tokenCredential = new VisualStudioCredential(credentialOptions);
+
+var clientOptions = new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+ServiceBusClient client = new ServiceBusClient(
+    "pichidazureservicebus.servicebus.windows.net",
+    tokenCredential,
+    clientOptions);
+
+ServiceBusSender sender = client.CreateSender("testqueue");
+
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+if (!messageBatch.TryAddMessage(new ServiceBusMessage("Test Message")))
+{
+    throw new Exception("The message is too large to fit in the batch.");
+}
+
+try
+{
+    await sender.SendMessagesAsync(messageBatch);
+
+    Console.WriteLine("A batch of message has been published to the queue.");
+}
+catch (ServiceBusException exception)
+{
+    Console.WriteLine($"ServiceBusException: {exception.Message}");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Press any key to end the application");
+Console.ReadKey();
---
+++ Pichid.AzureServiceBus.ConsoleReceiver/Program.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+var credentialOptions = new VisualStudioCredentialOptions
+{
+    TenantId = "3b4746c8-cc8f-4413-8e2c-5dad5c063418"
+};
+TokenCredential tokenCredential = new VisualStudioCredential(credentialOptions);
+
+var clientOptions = new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+ServiceBusClient client = new ServiceBusClient(
+    "pichidazureservicebus.servicebus.windows.net",
+    tokenCredential,
+    clientOptions);
+
+ServiceBusProcessor processor = client.CreateProcessor("testqueue");
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("Stopping the receiver...");
+
+    await processor.StopProcessingAsync();
+
+    Console.WriteLine("Stopped receiving messages");
+}
+catch (Exception exception)
+{
+    Console.WriteLine($"ServiceBusException: {exception.Message}");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+async Task MessageHandler(ProcessMessageEventArgs arg)
+{
+    string body = arg.Message.Body.ToString();
+
+    Console.WriteLine($"Received: {body}");
+
+    await arg.CompleteMessageAsync(arg.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs arg)
+{
+    Console.WriteLine(arg.Exception.ToString());
+
+    return Task.CompletedTask;
+}
